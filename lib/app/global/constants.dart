952df80fc494e3e3bdd9c1b455f28faf7d09254e
Benjamim Rodrigo <contato@benjamimrodrigo.com>
2025-01-28 18:02:29 -0300

    fix: changed ip to access api in local

--- lib/app/global/constants.dart
+++ lib/app/global/constants.dart
@@ -1,5 +1,5 @@
 
-const String API_HOST = "http://127.0.0.1:8080";
+const String API_HOST = "http://10.0.2.2:8080";
 const String API_USER = "wktest";
 const String API_PASS = "wktest";
 const String LOCAL_DATA = "assets/localdata/data.json";
